8e21430a7c559e713dfdd12267696dbad6474b8a
nguyenvanlich <nguyenvanlich15061994@gmailcom>
2017-11-07 20:34:53 +0700

    Tesst

--- QuanLyNhanSu/QuanLyNhanSu/GUI/FrmChamCong.cs
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmChamCong.cs
@@ -14,46 +14,7 @@ namespace QuanLyNhanSu.GUI
     public partial class FrmChamCong : Form
     {
 
-        private QuanLyNhanSuDbContext db = DAO.DBService.db;
-        private NHANVIEN nhanvien = new NHANVIEN();
 
-        #region constructor
-        public FrmChamCong()
-        {
-            InitializeComponent();
-            DAO.DBService.Reload();
-        }
-
-        public FrmChamCong(NHANVIEN nv)
-        {
-            InitializeComponent();
-            DAO.DBService.Reload();
-            nhanvien = nv;
-        }
-        #endregion
-
-        #region LoadForm
-        private void InitControl()
-        {
-            GroupThongTin.Enabled = false;
-            CbxLoai.SelectedIndex = 0;
-            dateBatDau.DateTime = DateTime.Now.AddMonths(-6);
-            dateKetThuc.DateTime = DateTime.Now;
-            DateNgay.DateTime = DateTime.Now;
-        }
-        private void LoadPhongBan()
-        {
-            int i = 1;
-            dgvPhongBanMain.DataSource = db.PHONGBANs.ToList()
-                                         .Where(p => p.ID == nhanvien.PHONGBANID || p.IDCAPTREN == nhanvien.PHONGBANID)
-                                         .Select(p => new
-                                         {
-                                             ID = p.ID,
-                                             STT = i++,
-                                             TenPB = p.TEN
-                                         }).ToList();
-            LoadNhanVien();
-        }
 
     }
 }
