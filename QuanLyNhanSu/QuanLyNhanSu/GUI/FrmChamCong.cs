3887e19424b21cc8b3b4582c6f2081d2981cd2a7
nguyenvanlich <nguyenvanlich15061994@gmail.com>
2017-11-11 08:49:50 +0700

    FrmChamCong3

--- QuanLyNhanSu/QuanLyNhanSu/GUI/FrmChamCong.cs
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmChamCong.cs
@@ -109,5 +109,69 @@ namespace QuanLyNhanSu.GUI
         }
         #endregion
 
+        #region Hàm chức năng
+        private bool CheckChamCong()
+        {
+            CHAMCONG cc = GetChamCongWithGroupThongTin();
+
+            NHANVIEN nv = GetNhanVienWithID();
+            int iz;
+            if (btnThem.Enabled == true) iz = 0; else iz = nv.ID;
+
+            int cnt = (int)db.CHAMCONGs.ToList().Where(p => p.NHANVIENID == cc.NHANVIENID && p.NGAY == cc.NGAY && p.NHANVIENID != iz).ToList().Count;
+
+
+            if (cnt > 0)
+            {
+                MessageBox.Show("Nhân viên này đã được chấm công ngày " + ((DateTime)cc.NGAY).ToString("dd/MM/yyyy"), "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+        private void UpdateDetailChamCong()
+        {
+            CHAMCONG cc = GetChamCongWithID();
+            DateNgay.DateTime = (DateTime)cc.NGAY;
+            CbxLoai.SelectedIndex = (int)cc.LOAI;
+        }
+        private NHANVIEN GetNhanVienWithID()
+        {
+            try
+            {
+                int ID = (int)dgvNhanVienView.GetFocusedRowCellValue("ID");
+                NHANVIEN ans = db.NHANVIENs.Where(p => p.ID == ID).FirstOrDefault();
+                return ans;
+            }
+            catch
+            {
+                return new NHANVIEN();
+            }
+        }
+        private CHAMCONG GetChamCongWithID()
+        {
+            try
+            {
+                int ID = (int)dgvChamCongView.GetFocusedRowCellValue("ID");
+                CHAMCONG ans = db.CHAMCONGs.Where(p => p.ID == ID).FirstOrDefault();
+                return ans;
+            }
+            catch
+            {
+                return new CHAMCONG();
+            }
+        }
+
+        private CHAMCONG GetChamCongWithGroupThongTin()
+        {
+            CHAMCONG ans = new CHAMCONG();
+            ans.NHANVIENID = GetNhanVienWithID().ID;
+            ans.NGAY = DateNgay.DateTime;
+            ans.LOAI = CbxLoai.SelectedIndex;
+            return ans;
+        }
+        #endregion
+
+
     }
 }
