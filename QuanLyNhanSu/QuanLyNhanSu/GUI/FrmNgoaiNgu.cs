e4d9cb04fa41f47613ff1ddba85cc6be3ea580c9
windatm <tuanphongmta0205@gmail.com>
2017-11-11 08:32:51 +0700

    FrmNgoaiNgu1

--- QuanLyNhanSu/QuanLyNhanSu/GUI/FrmNgoaiNgu.cs
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmNgoaiNgu.cs
@@ -14,10 +14,50 @@ namespace QuanLyNhanSu.GUI
     public partial class FrmNgoaiNgu : Form
     {
 
+        private QuanLyNhanSuDbContext db = DBService.db;
+        private int index = 0;
+        private int index1 = 0;
+
         #region constructor
         public FrmNgoaiNgu()
         {
             InitializeComponent();
+            DBService.Reload();
+        }
+        #endregion
+
+        #region LoadForm
+
+        private void LoadDgvPhongBan()
+        {
+            int i = 1;
+            dgvChucVuMain.DataSource = db.NGOAINGUs.ToList().Select(p => new
+            {
+                STT = i++,
+                ID = p.ID,
+                TenNN = p.TEN
+            });
+
+            // chỉnh lại dòng thành dòng vừa chọn
+            try
+            {
+                index = index1;
+                dgvChucVu.FocusedRowHandle = index;
+
+            }
+            catch { }
+        }
+
+        private void LoadInitControl()
+        {
+            groupThongTin.Enabled = false;
+            btnHuy.Enabled = false;
+        }
+        private void FrmPhongBan_Load(object sender, EventArgs e)
+        {
+            LoadInitControl();
+            LoadDgvPhongBan();
+
         }
         #endregion
 
