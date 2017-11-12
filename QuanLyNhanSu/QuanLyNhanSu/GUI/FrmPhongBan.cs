3932382cf636288dcc15adfb1c5570463829be2d
taikhoana1 <noidovatoi11196@gmail.com>
2017-11-12 08:23:44 +0700

    Phong ban l1

--- QuanLyNhanSu/QuanLyNhanSu/GUI/FrmPhongBan.cs
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmPhongBan.cs
@@ -15,14 +15,66 @@ namespace QuanLyNhanSu.GUI
     {
 
 
+        private QuanLyNhanSuDbContext db = DBService.db;
+        private int index = 0;
+        private int index1 = 0;
+
         #region constructor
         public FrmPhongBan()
         {
             InitializeComponent();
-           
+            DBService.Reload();
         }
         #endregion
 
+        #region LoadForm
+
+        private void LoadDgvPhongBan()
+        {
+            int i = 1;
+            dgvPhongBanMain.DataSource = db.PHONGBANs.ToList().Select(p => new
+            {
+                STT = i++,
+                ID = p.ID,
+                TenPB = p.TEN,
+                CapTren = p.IDCAPTREN == null ? "Không" : db.PHONGBANs.Where(ct => ct.ID == p.IDCAPTREN).FirstOrDefault().TEN
+            });
+
+            // chỉnh lại dòng thành dòng vừa chọn
+            try
+            {
+                index = index1;
+                dgvPhongBan.FocusedRowHandle = index;
+
+            }
+            catch { }
+        }
+
+        private void LoadInitControl()
+        {
+            List<PHONGBAN> lpb = new List<PHONGBAN>();
+            lpb = db.PHONGBANs.Select(p => p).ToList();
+
+            PHONGBAN pb = new PHONGBAN();
+            pb.ID = 0;
+            pb.TEN = "Không";
+            lpb.Add(pb);
+
+            /// cbx Cap Tren
+            cbxCapTren.DataSource = lpb;
+            cbxCapTren.DisplayMember = "TEN";
+            cbxCapTren.ValueMember = "ID";
+
+            groupThongTin.Enabled = false;
+            btnHuy.Enabled = false;
+        }
+        private void FrmPhongBan_Load(object sender, EventArgs e)
+        {
+            LoadInitControl();
+            LoadDgvPhongBan();
+
+        }
+        #endregion
 
     }
 }
