720086a69b60a4ee94ef1a4691cbc3c479ff319a
windatm <tuanphongmta0205@gmail.com>
2017-11-13 14:53:27 +0700

    FrmQuanHeGiaDinh2

--- QuanLyNhanSu/QuanLyNhanSu/GUI/FrmQuanHeGiaDinh.cs
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmQuanHeGiaDinh.cs
@@ -61,6 +61,111 @@ namespace QuanLyNhanSu.GUI
         }
         #endregion
 
+        #region sự kiện ngầm
+        private void dgvPhongBan_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            try
+            {
+                CapNhatDetail();
+
+                index1 = index;
+                index = dgvView.FocusedRowHandle;
+            }
+            catch
+            {
+            }
+        }
+        #endregion
+
+        #region Hàm chức năng
+
+        /// <summary>
+        /// Cập nhật lại thông tin của detail khi có sự thay đổi ở các dòng
+        /// </summary>
+        private void CapNhatDetail()
+        {
+            try
+            {
+                QUANHEGD tg = GetThongTin();
+
+                txtTen.Text = tg.TEN;
+            }
+            catch
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// check xem thông tin người dùng nhập vào có chính xác không
+        /// </summary>
+        /// <returns></returns>
+        private bool Check()
+        {
+            if (txtTen.Text == "")
+            {
+                MessageBox.Show("Tên quan hệ gia đình không được để trống", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Cập nhật lại trạng thái của form
+        /// </summary>
+        private void Update()
+        {
+            LoadInitControl();
+            LoadDgvPhongBan();
+        }
+
+        /// <summary>
+        /// Lấy ra phòng ban được lựa chọn từ ID
+        /// </summary>
+        /// <returns> Phòng ban </returns>
+        private QUANHEGD GetThongTin()
+        {
+            QUANHEGD ans = new QUANHEGD();
+            ans.ID = 0;
+
+            try
+            {
+                int id = (int)dgvView.GetFocusedRowCellValue("ID");
+                QUANHEGD tg = db.QUANHEGDs.Where(p => p.ID == id).FirstOrDefault();
+                return tg;
+            }
+            catch
+            {
+            }
+
+            return ans;
+        }
+
+        /// <summary>
+        /// get thông tin phòng ban từ group
+        /// </summary>
+        /// <returns></returns>
+        private QUANHEGD GetTTNhap()
+        {
+            QUANHEGD tg = new QUANHEGD();
+
+            try
+            {
+                tg.ID = (int)dgvView.GetFocusedRowCellValue("ID");
+            }
+            catch { }
+
+            tg.TEN = txtTen.Text;
+
+            return tg;
+        }
+
+        private void ClearControl()
+        {
+            txtTen.Text = "";
+        }
+        #endregion
 
 
     }
