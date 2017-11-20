046c0d70db45184db827e7146d378d90f40e941c
taikhoana1 <noidovatoi11196@gmail.com>
2017-11-20 15:03:53 +0700

    HocVan l3

--- QuanLyNhanSu/QuanLyNhanSu/GUI/FrmTrinhDoHocVan.cs
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmTrinhDoHocVan.cs
@@ -75,7 +75,95 @@ namespace QuanLyNhanSu.GUI
             }
         }
         #endregion
+        #region Hàm chức năng
 
+        /// <summary>
+        /// Cập nhật lại thông tin của detail khi có sự thay đổi ở các dòng
+        /// </summary>
+        private void CapNhatDetail()
+        {
+            try
+            {
+                TRINHDOHOCVAN tg = GetThongTin();
+
+                txtTen.Text = tg.TEN;
+            }
+            catch
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// check xem thông tin người dùng nhập vào có chính xác không
+        /// </summary>
+        /// <returns></returns>
+        private bool Check()
+        {
+            if (txtTen.Text == "")
+            {
+                MessageBox.Show("Tên trình độ học vấn không được để trống", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Cập nhật lại trạng thái của form
+        /// </summary>
+        private void Update()
+        {
+            LoadInitControl();
+            LoadDgvPhongBan();
+        }
+
+        /// <summary>
+        /// Lấy ra phòng ban được lựa chọn từ ID
+        /// </summary>
+        /// <returns> Phòng ban </returns>
+        private TRINHDOHOCVAN GetThongTin()
+        {
+            TRINHDOHOCVAN ans = new TRINHDOHOCVAN();
+            ans.ID = 0;
+
+            try
+            {
+                int id = (int)dgvView.GetFocusedRowCellValue("ID");
+                TRINHDOHOCVAN tg = db.TRINHDOHOCVANs.Where(p => p.ID == id).FirstOrDefault();
+                return tg;
+            }
+            catch
+            {
+            }
+
+            return ans;
+        }
+
+        /// <summary>
+        /// get thông tin phòng ban từ group
+        /// </summary>
+        /// <returns></returns>
+        private TRINHDOHOCVAN GetTTNhap()
+        {
+            TRINHDOHOCVAN tg = new TRINHDOHOCVAN();
+
+            try
+            {
+                tg.ID = (int)dgvView.GetFocusedRowCellValue("ID");
+            }
+            catch { }
+
+            tg.TEN = txtTen.Text;
+
+            return tg;
+        }
+
+        private void ClearControl()
+        {
+            txtTen.Text = "";
+        }
+        #endregion
 
 
     }
