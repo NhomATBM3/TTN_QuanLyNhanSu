9e5dbc60568d84e5c7bcee6afd184f7348b269e1
nguyenvanlich <nguyenvanlich15061994@gmail.com>
2017-11-25 15:15:23 +0700

    FrmLuongCoBan1

--- QuanLyNhanSu/QuanLyNhanSu/GUI/FrmLuongCoBan.cs
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmLuongCoBan.cs
@@ -12,15 +12,24 @@ namespace QuanLyNhanSu.GUI
 {
     public partial class FrmLuongCoBan : Form
     {
-        
+        private QuanLyNhanSuDbContext db = DAO.DBService.db;
 
         #region constructor
         public FrmLuongCoBan()
         {
             InitializeComponent();
-            
+            DAO.DBService.Reload();
         }
         #endregion
+
+        #region LoadForm
+        private void FrmLuongCoBan_Load(object sender, EventArgs e)
+        {
+            THAMSOHETHONG luongcoban = db.THAMSOHETHONGs.FirstOrDefault();
+            txtLuongCoBan.Text = luongcoban.VALUE.ToString();
+        }
+        #endregion
+
 
 
 
